ae9f97dc520b8ee0de714c834608f83bfed675c2
Hetang Mehta <112903225+Killinwraith@users.noreply.github.com>
2022-12-05 14:48:41 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+/*
+document.addEventListener("DOMContentLoaded",() => {
+    const loginForm = document.querySelector("#userlogin");
+    const createAccountForm = document.querySelector("#CreateAcc");
+
+    document.querySelector("#linkCreateAcc").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.add("subframe--hidden");
+        createAccountForm.classList.remove("subframe--hidden");
+        return;
+    });
+
+    document.querySelector("#linkuserlogin").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.remove("subframe--hidden");
+        createAccountForm.classList.add("subframe--hidden");
+        return;
+    });
+
+});
+
+*/
+
